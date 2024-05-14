195edb2aef88b8b22be52a78056157dc6086acc5
Gabriel Vitor <branco477@outlook.com>
2024-05-14 16:12:23 -0300

    visualizar contato

---
+++ show.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	include_once("templates/header.php");
+	include_once("dao/contactDAO.php");
+
+	$contactsDAO = new Contacts($conn);
+
+	$viewContact = $contactsDAO->findBy($_GET["id"]);
+
+?>
+
+	<div class="container" id="view-contact-container">
+		<h1 id="main-title"><?= $viewContact["name"] ?></h1>
+		<p class="bold">Telefone: </p>
+		<p><?= $viewContact["phone"] ?></p>
+		<p class="bold">Observações: </p>
+		<p><?= $viewContact["observations"] ?></p>
+	</div>
+
+<?php 
+	include_once("templates/footer.php");
+?>
