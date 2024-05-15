2044600af9f69b638ae1a207d57601f8615451a4
Gabriel Vitor <branco477@outlook.com>
2024-05-15 02:52:22 -0300

    arrumando css do create

--- create.php
+++ create.php
@@ -3,24 +3,26 @@
 
  ?>
 
-<h1>Página Create</h1>
-
-<form action="config/process.php" method="POST">
-	<input type="hidden" name="create" value="type">
-	<div>
-		<input type="text" name="name" placeholder="Digite o nome:" required>	
+	<div class="container" id="create-contact-container">
+		<?php include_once("templates/backbtn.html") ?>
+		<h1 id="main-title">Criar contato</h1>
+		<form action="config/process.php" method="POST">
+			<input type="hidden" name="type" value="create">
+			<div class="form-group">
+				<label for="name">Nome do contato:</label>
+				<input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome:" required>	
+			</div>
+			<div class="form-group">
+				<label for="phone">Telefone:</label>
+				<input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone:" required>
+			</div>
+			<div class="form-group">
+				<label for="phone">Observação:</label>
+				<textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira observações:" rows="3" required></textarea>
+			</div>
+			<button type="submit" class="btn btn-primary">Cadastrar</button>
+		</form>
 	</div>
-	<div>
-		<input type="text" name="phone" placeholder="Digite o telefone:" required>
-	</div>
-	<div>
-		<textarea> name="observations" placeholder="Observação" required></textarea>
-	</div>
-	<div>
-		<input type="submit" value="Enviar">
-	</div>
-</form>
-
 <?php 
 	include_once("templates/footer.php");
 ?> 
