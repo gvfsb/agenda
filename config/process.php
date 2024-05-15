fcc3a64d36035fbe0c392f40990c5c0af8e1e6de
Gabriel Vitor <branco477@outlook.com>
2024-05-15 04:46:35 -0300

    Adicionando atualização de contatos.

--- config/process.php
+++ config/process.php
@@ -6,24 +6,35 @@
 	include_once("url.php");
 	include_once(__DIR__.'/../dao/contactDAO.php');
 
-	if (!empty($_POST["form-login"])) {
-		$_SESSION["msg"] = "Conectado com sucesso.";
-		header("Location: ../index.php");	
-	}
+	$contactDAO = new Contacts($conn);
 
-	if (!empty($_POST["type"])) {
+	if (!empty($_POST["type"]) && $_POST["type"] === "create") {
 		
 		$name = $_POST['name'];
 		$phone = $_POST['phone'];
-		$observation = $_POST['observations'];
+		$observations = $_POST['observations'];
 
-		$contactDAO = new Contacts($conn);
-
-		$contactDAO->create($name, $phone, $observation);
+		$contactDAO->create($name, $phone, $observations);
 
 		$_SESSION["msg"] = "Contato criado com sucesso.";
 
 		header("Location: ../index.php");
+
+	}
+
+	if (!empty($_POST["type"]) && $_POST["type"] === "update") {
+		
+		$id = $_POST["id"];
+		$name = $_POST['name'];
+		$phone = $_POST['phone'];
+		$observations = $_POST['observations'];
+
+		$contactDAO->update($id, $name, $phone, $observations);
+
+		$_SESSION["msg"] = "Contato atualizado com sucesso.";
+
+		header("Location: ../index.php");
+
 
 	}
 
