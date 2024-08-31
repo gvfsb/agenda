7c53761fe3644c4b323b95770e3a1eb670aae994
Gabriel <branco477@outlook.com>
2024-08-31 16:03:59 -0300

    criação do controller do projeto

---
+++ controllers/contact.php
@@ -0,0 +1,100 @@
+<?php
+
+  include_once(__DIR__.'/../models/contact.php');
+
+  class ContactDAO{
+
+      private $conn;
+      private $name;
+      private $phone;
+      private $observations;
+
+      public function __construct(PDO $conn){
+        $this->conn = $conn;
+      }
+
+      public function create(Contact $contact){
+        
+        $stmt = $this->conn->prepare("INSERT INTO contacts(name_user, data_user, observations) VALUES(:name, :phone, :observations)");
+
+        $stmt->bindParam(":name", $contact->getName());
+        $stmt->bindParam(":phone", $contact->getPhone());
+        $stmt->bindParam(":observations", $contact->getObservations());
+
+        $stmt->execute();
+
+      }
+
+      public function findAll(){
+
+        $stmt = $this->conn->prepare("SELECT * FROM contacts");
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+
+        return $contacts;
+      }
+
+      public function findBy($id){
+
+        $stmt = $this->conn->prepare("SELECT * FROM contacts WHERE id = :id");
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+
+        return $contact;
+
+      }
+
+      public function update(Contact $contact){
+
+        $stmt = $this->conn->prepare("UPDATE contacts SET name_user = :name, data_user = :phone, observations = :observations WHERE id = :id");
+
+        $stmt->bindParam(":id", $contact->getId());
+        $stmt->bindParam(":name", $contact->getName());
+        $stmt->bindParam(":phone", $contact->getPhone());
+        $stmt->bindParam(":observations", $contact->getObservations());
+
+        $stmt->execute();
+
+      }
+
+      public function delete(Contact $contact){
+
+        $stmt = $this->conn->prepare("DELETE FROM contacts WHERE id = :id");
+
+        $stmt->bindParam(":id", $contact->getId());
+
+        $stmt->execute();
+
+      } 
+
+      public function setName(string $name){
+        $this->name = $name;
+      }
+
+      public function getName(){
+        return $this->name;
+      }
+
+      public function setPhone(int $phone){
+        $this->phone;
+      }
+
+      public function getPhone(){
+        return $this->phone;
+      }
+
+      public function setObservations(string $observations){
+        $this->observations = $observations;
+      }
+
+      public function getObservations(){
+        return $this->observations;
+      }
+
+    }
