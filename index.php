40bcf2964a292e6609b6f785b9a2f90b5f7e5b3a
Gabriel <branco477@outlook.com>
2024-09-17 14:17:15 -0300

    Chamando url base

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
       						<td scope="row"><?= $contact["name_user"] ?></td>
       						<td scope="row"><?= $contact["data_user"] ?></td>
       						<td class="actions">
-      							<a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon"></i></a>
-      							<a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
+      							<a href="<?= BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon"></i></a>
+      							<a href="<?= BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
       							<form class="delete-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
       								<input type="hidden" name="type" value="delete" >
       								<input type="hidden" name="id" value="<?= $contact["id"] ?>">
