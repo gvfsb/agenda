eb0ee570059d28e3345db0de745c982c121bfb0a
Gabriel <branco477@outlook.com>
2024-09-18 11:45:25 -0300

    Removendo includes desnecessários

--- show.php
+++ show.php
@@ -2,10 +2,7 @@
 	
 	use App\Controllers\Contato;
 
-	require __DIR__ . '/vendor/autoload.php';
 	include_once("views/includes/header.php");
-	include_once(__DIR__.'/config/bd.php');
-
 
 	$contactsDAO = new Contato($conn);
 
